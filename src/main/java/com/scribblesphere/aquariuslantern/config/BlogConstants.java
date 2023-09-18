1689687a3c3a44ac0abc99794099c1df5fae0e8d
abrar <abrar.sheikh@flytxt.com>
2023-09-18 16:07:12 +0530

    Added Comment and Roles to users as well as spring security feature.

---
+++ src/main/java/com/scribblesphere/aquariuslantern/config/BlogConstants.java
@@ -0,0 +1,10 @@
+package com.scribblesphere.aquariuslantern.config;
+
+public class BlogConstants {
+
+    public static final String PAGE_NUMBER = "0";
+    public static final String PAGE_SIZE = "10";
+    public static final String SORT_BY = "postId";
+    public static final String DIRECTION = "ASC";
+
+}
